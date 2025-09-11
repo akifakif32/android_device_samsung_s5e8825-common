64797805c039afc0fc2a0bd0c4272f57f0d680c0
Majaahh <maja.blaszczak@proton.me>
2025-09-11 22:24:42 +0200

    s5e8825-common: Enable offline charging suspend
    * linux4 said that it doesn't cause kernel panics on A16 anymore
    
    Change-Id: I79ce0b5443c3491a7b12acf7b3f02b7525b6653a
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -147,8 +147,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.samsung \
     android.hardware.health-service.samsung-recovery
 
-$(call soong_config_set,samsungHealthVars,enable_suspend,false)
-
 # Init
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/init/fstab.s5e8825:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/fstab.s5e8825 \
