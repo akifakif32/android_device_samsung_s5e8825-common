2be0af90a2fa1f69814eee2a70c0e8ce50dd3890
akifakif32 <mhmtkfdrn@gmail.com>
2025-11-10 18:28:48 +0200

    Revert "s5e8825-common: Don’t explicitly build shims"
    This reverts commit d3e4b274e618ddfbfad814bb7a93c78aeb78c9fb.

--- common.mk
+++ common.mk
@@ -79,6 +79,10 @@ PRODUCT_PACKAGES += init.s5e8825.bluetooth.rc
 # Camera
 PRODUCT_PACKAGES += android.hardware.camera.provider-service.samsung
 
+PRODUCT_PACKAGES += \
+    libvpl \
+    libshim_camera
+
 $(call soong_config_set,samsungCameraVars,extra_ids,60)
 $(call soong_config_set,samsungCameraVars,needs_sec_reserved_field,true)
 $(call soong_config_set,samsungCameraVars,usage_64bit,true)
@@ -181,6 +185,8 @@ PRODUCT_PACKAGES += \
     lib_android_keymaster_keymint_utils.vendor \
     libcppbor_external.vendor \
     libkeymint.vendor
+
+PRODUCT_PACKAGES += libshim_crypto
 
 # Kernel Modules
 PRODUCT_PACKAGES += toolbox.vendor_ramdisk
@@ -305,6 +311,7 @@ PRODUCT_PACKAGES += SamsungDAP-custom
 
 # Sensors
 PRODUCT_PACKAGES += android.hardware.sensors-service.samsung-multihal
+PRODUCT_PACKAGES += libshim_sensorndkbridge
 
 # Sensors - Init
 PRODUCT_PACKAGES += init.s5e8825.sensors.rc
