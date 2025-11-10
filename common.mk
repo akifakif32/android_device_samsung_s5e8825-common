f11e037e80052a1a893d502b766bada7714a0c26
akifakif32 <mhmtkfdrn@gmail.com>
2025-11-11 02:20:20 +0300

    Don’t explicitly build shims except lipvpl.so

--- common.mk
+++ common.mk
@@ -79,9 +79,7 @@ PRODUCT_PACKAGES += init.s5e8825.bluetooth.rc
 # Camera
 PRODUCT_PACKAGES += android.hardware.camera.provider-service.samsung
 
-PRODUCT_PACKAGES += \
-    libvpl \
-    libshim_camera
+PRODUCT_PACKAGES += libvpl
 
 $(call soong_config_set,samsungCameraVars,extra_ids,60)
 $(call soong_config_set,samsungCameraVars,needs_sec_reserved_field,true)
@@ -185,8 +183,6 @@ PRODUCT_PACKAGES += \
     lib_android_keymaster_keymint_utils.vendor \
     libcppbor_external.vendor \
     libkeymint.vendor
-
-PRODUCT_PACKAGES += libshim_crypto
 
 # Kernel Modules
 PRODUCT_PACKAGES += toolbox.vendor_ramdisk
@@ -311,7 +307,6 @@ PRODUCT_PACKAGES += SamsungDAP-custom
 
 # Sensors
 PRODUCT_PACKAGES += android.hardware.sensors-service.samsung-multihal
-PRODUCT_PACKAGES += libshim_sensorndkbridge
 
 # Sensors - Init
 PRODUCT_PACKAGES += init.s5e8825.sensors.rc
