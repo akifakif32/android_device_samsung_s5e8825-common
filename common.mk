f976371e3073ea4af0013ef85dba4d80c0f2ca98
akifakif32 <mhmtkfdrn@gmail.com>
2026-01-19 12:18:51 +0300

    s5e8825-common: Fix build errors with updated hardware/samsung Update camera variables to use soong_config_set_bool to match the latest hardware/samsung requirements.

--- common.mk
+++ common.mk
@@ -80,8 +80,8 @@ PRODUCT_PACKAGES += init.s5e8825.bluetooth.rc
 PRODUCT_PACKAGES += android.hardware.camera.provider-service.samsung
 
 $(call soong_config_set,samsungCameraVars,extra_ids,60)
-$(call soong_config_set,samsungCameraVars,needs_sec_reserved_field,true)
-$(call soong_config_set,samsungCameraVars,usage_64bit,true)
+$(call soong_config_set_bool,samsungCameraVars,needs_sec_reserved_field,true)
+$(call soong_config_set_bool,samsungCameraVars,usage_64bit,true)
 
 # Camera - Init
 PRODUCT_PACKAGES += init.s5e8825.camera.rc
